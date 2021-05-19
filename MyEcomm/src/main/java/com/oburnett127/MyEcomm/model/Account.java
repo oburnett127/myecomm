e7681e70e2d6d9441dc66266f07931234fbf2834
oburn <oburn@DESKTOP-9SGSSQ4>
2021-05-19 11:28:27 -0500

    Trying to fix constructor related lombok problem, the all args constructor for the ServiceError class is not working. Also need to fix the multivaluemap cannot be resolved error. Commiting so that I can delete and recreate the project locally.

--- MyEcomm/src/main/java/com/oburnett127/MyEcomm/model/Account.java
+++ MyEcomm/src/main/java/com/oburnett127/MyEcomm/model/Account.java
@@ -2,11 +2,15 @@ package com.oburnett127.MyEcomm.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Account {
 	private int accountId;
 	private String email;
